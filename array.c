2f515350f0492ca927b0ac853360a68366c1f073
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-23 16:03:02 +0530

    array.c
    good

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("\n Enter the number n integer");
+scanf("%d,&i,%n);
+{
+array==1;
+i==6;
+}
+printf("\n enter the number");
+{
+for(i=2;i<n;i++)
+}
+return 0;
+}
